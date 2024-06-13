26a87bfc1e9041a5669b45ac5d171b9dad3195b3
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-06-13 12:47:39 -0600

    client routes added

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main>
+      <Outlet />
+    </main>
+  )
+}
+
+export default App
